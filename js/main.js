61ef111adfe6f9d39f9a2419ec51a55ea0416b99
Olufifun <olufifun@gmail.com>
2019-08-26 14:50:38 +0100

    Development attempt for Parallax Effect

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
     window.addEventListener('scroll', function(e) {
+        const target = document.querySelector('.scroll');
+
+        var scrolled = window.pageYOffset;
+        var rate = scrolled * 0.5;
+
+        target.style.transform = "translate3d(0px, "+ rate + "px, 0px)";
+        
 
     })
